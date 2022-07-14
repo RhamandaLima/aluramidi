07e52442f476c29eb7cf2b12ba02e1f995272aac
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-07-14 20:28:37 -0300

    feat: adding for

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla'); 
 
-let contador = 0; 
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -17,6 +15,4 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
-    
-    contador = contador + 1; 
 }
